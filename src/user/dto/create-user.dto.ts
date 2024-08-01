5c38ed48cacc015f7841c8471e66b718e32ad197
Ashraf22 <ashrafmaged593@gmail.com>
2024-08-01 17:09:03 +0300

    [refactor] sheikh model to user model and add roles

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export enum Role {
+  ADMIN = 'admin',
+  SHEIKH = 'sheikh',
+  STUDENT = 'student',
+}
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @IsNotEmpty()
+  role: Role;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(data: CreateUserDto) {
+    const User = await this.prisma.user.create({
+      data,
+    });
+    return User;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, data: UpdateUserDto) {
+    const updateUser = await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+    return updateUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const deleteUser = await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+    return deleteUser;
   }
 }
