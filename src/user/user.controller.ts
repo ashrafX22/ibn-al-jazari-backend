6f969874df2fa9631d8745e8c4057923ca57078c
Ashraf22 <ashrafmaged593@gmail.com>
2024-08-03 16:20:47 +0300

    [fix] take out the password feild from the response object

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,13 +8,18 @@ import {
   Delete,
   UsePipes,
   ValidationPipe,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 @UsePipes(new ValidationPipe())
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -29,7 +34,7 @@ export class UserController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<UserEntity> {
     return this.userService.findOne(+id);
   }
 
