c56df89b8f38e878f365b0b6163aa1c80a325fb2
HamzaA598 <hamzaemad598@gmail.com>
2024-08-19 00:11:19 +0300

    [update] access and refresh tokens upon login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { StudentService } from 'src/student/student.service';
 import { TeacherService } from 'src/teacher/teacher.service';
+import { IUser } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
     constructor(private studentService: StudentService, private teacherService: TeacherService) { }
 
-    async validateUser(email: string) {
+    async validateUser(access_token: string, refresh_token: string, email: string) {
         console.log("auth service")
-        const student = await this.teacherService.findByEmail(email);
-        const teacher = await this.studentService.findByEmail(email);
+        const services: IUser[] = [this.studentService, this.teacherService];
 
-        if (!student && !teacher) return null;
-        return student || teacher;
+        for (const service of services) {
+            const user = await service.findByEmail(email);
+            if (user) {
+                await service.update(
+                    user.id,
+                    {
+                        access_token,
+                        refresh_token: refresh_token || ''
+                    }
+                );
+                return user;
+            }
+        }
+
+        return null;
     }
 
     async get(id: number) {
