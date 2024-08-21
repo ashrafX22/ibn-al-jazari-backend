32d5f0c37af8ce07e5b31a62339ef4f2a90e4c11
Ashraf22 <ashrafmaged593@gmail.com>
2024-08-22 01:08:32 +0300

    [add] @api property for the teacher enity

--- src/teacher/entities/teacher.entity.ts
+++ src/teacher/entities/teacher.entity.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { Role } from 'src/models/enums/role.enum';
 import { UserEntity } from 'src/user/entities/user.entity';
 
 export class TeacherEntity extends UserEntity {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   summary: string;
+  @ApiProperty()
   proficiencyLevel: Role;
 
   constructor(partial: Partial<TeacherEntity>) {
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,20 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { calculateAge } from 'src/shared/utils/calculate-age.util';
 
 export class UserEntity {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   dateOfBirth: Date;
+  @ApiProperty()
   phoneNumber: string;
+  @ApiProperty()
   gender: string;
+
+  @ApiProperty()
+  profilePicture: string;
 
   @Exclude()
   password: string;
+
   @Exclude()
   accessToken: string;
+
   @Exclude()
   refreshToken: string;
 
+  @ApiProperty()
   @Expose()
   get age(): number {
     return calculateAge(this.dateOfBirth);
