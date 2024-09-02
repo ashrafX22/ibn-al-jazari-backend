4097f6faee491f7194e9277e6330a18c81d36a24
HamzaA598 <hamzaemad598@gmail.com>
2024-09-03 02:06:30 +0300

    [update] Matches to IsDateString

--- src/modules/appointment/dto/create-appointment.dto.ts
+++ src/modules/appointment/dto/create-appointment.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { IsDateString, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { Day } from 'src/models/enums/day.enum';
 
 export class CreateAppointmentDto {
@@ -13,9 +13,7 @@ export class CreateAppointmentDto {
     description: 'Start time in HH:mm format',
   })
   @IsString()
-  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
-    message: 'startTime must be in the format HH:mm',
-  })
+  @IsDateString()
   @IsNotEmpty()
   startTime: string;
 }
