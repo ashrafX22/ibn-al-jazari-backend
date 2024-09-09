298741697248c72ce302fbc557845dc6b216a940
HamzaA598 <hamzaemad598@gmail.com>
2024-09-10 01:57:18 +0300

    [fix] flatten-object when receiving null value

--- src/shared/utils/flatten-object.util.ts
+++ src/shared/utils/flatten-object.util.ts
@@ -2,7 +2,7 @@ export function flattenObject(obj: Object): Object {
     let result = {}
 
     for (let [key, value] of Object.entries(obj)) {
-        if (typeof (value) !== 'object' || value instanceof Date)
+        if (typeof (value) !== 'object' || value instanceof Date || !value)
             result = { ...result, [key]: value }
         else
             result = { ...result, ...flattenObject(value) };
